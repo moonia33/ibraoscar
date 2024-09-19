71818df1cf9ae96fa417f2ffa771375013c36835
Ramūnas <ramunas@inultimo.lt>
2024-09-19 19:49:58 +0300

    children+montonio

--- ibra/custom_checkout/apps.py
+++
@@ -1,6 +0,0 @@
-# custom_checkout/apps.py
-from oscar.apps.checkout.apps import CheckoutConfig
-
-
-class CustomCheckoutConfig(CheckoutConfig):
-    name = 'custom_checkout'
--- ibra/custom_checkout/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('payment-details/', views.payment_details_view, name='payment-details'),
-]
